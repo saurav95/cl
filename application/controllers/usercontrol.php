8ba4c93f326630363cefbde95c237665a887dbe5
saurav95 <passiontowork222@gmail.com>
2016-05-26 06:08:25 -0700

    Frame Work

---
+++ application/controllers/usercontrol.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Usercontrol extends CI_Controller {
+	
+	public function index() {
+		
+		$this->load->view('user');
+		
+	}
+	
+	
+}
+
+
+?>
---
+++ application/models/userdata.php
@@ -0,0 +1,15 @@
+<?php 
+
+class User_data extends CI_Model {
+	
+	public function getUser() {
+		
+		return[
+		   ["FirstName" => "Saurav" , "LastName" => "Bisht"];
+		]
+		
+	}
+	
+}
+
+?>
